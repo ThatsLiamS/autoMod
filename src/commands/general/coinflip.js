18295ae412bf53eddf8fad401939a299bf910fdf
ThatsLiamS <73022526+ThatsLiamS@users.noreply.github.com>
2021-05-02 14:28:35 +0100

    Create coinflip.js

---
+++ src/commands/general/coinflip.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+module.exports = {
+    name: 'coinflip',
+    description: 'Flip a coin! Very good for making decisions.', 
+    aliases: ["cf", "coinflipper"],
+    async execute(message, args, prefix, client, firestore){
+        
+        const flip = Math.floor((Math.random() * 100) + 0);
+        let ans = ``
+
+        if(flip % 2 == 0) ans = `heads`
+        else ans = `tails`
+
+        if(flip == 73) ans = `its side! Impressive`
+   
+        const embed = new Discord.MessageEmbed()
+        .setTitle(`Coin Flipper`)
+        .setDescription(`The coin landed on ${ans}\n\nPowered by [@CoinFlipper#1767](https://coinflipperbot.glitch.me/)`)
+        .setColor(`#cd7f32`)
+        .setThumbnail(`https://i.imgur.com/4Zw6qky.png`)
+        message.channel.send(embed).catch(() => { message.author.send(`I am unable to send messages in ${message.channel}, please move to another channel`)}).catch(() => { })
+
+        client.developer.get('logs').execute(client, message, "Coin Flip Command")
+    }
+}
