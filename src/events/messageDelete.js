9d5fbef90b3222eed5abff9fb19ad322d8ba0dc3
ThatsLiamS <73022526+ThatsLiamS@users.noreply.github.com>
2021-05-02 13:31:13 +0100

    Delete messageDelete.js

--- src/events/messageDelete.js
+++
@@ -1,7 +0,0 @@
-const GhostPing = require('discord.js-ghost-ping');
-module.exports = {
-	name: 'messageDelete',
-	async execute(message, client) {
-		GhostPing.detector("messageDelete", message).catch(() => { });
-	}
-};
