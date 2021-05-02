6a91986adcf2dd893b0ee504c156c793e3036447
ThatsLiamS <73022526+ThatsLiamS@users.noreply.github.com>
2021-05-02 14:08:43 +0100

    Fixed Github formatting

--- src/commands/support/help.js
+++ src/commands/support/help.js
@@ -5,18 +5,18 @@ module.exports = {
         const member = await message.member
 
         const realhelp = new Discord.MessageEmbed()
-	    .setColor('#0099ff')
+	.setColor('#0099ff')
         .setTitle(`autoMod Commands`)
         .setThumbnail(client.user.displayAvatarURL())
         .setDescription(`To view the information about a certain Command do \n\`${prefix}help <command>\` For Example \`${prefix}help points\``)
-	    .addFields(
-		    { name: 'General Commands', value: `\`${prefix}serverinfo\` \`${prefix}userinfo\` \`${prefix}8ball\` \`${prefix}coinflip\` \`${prefix}say\``, inline: true },
-		    { name: 'Moderation Commands', value: `\`${prefix}clear\` \`${prefix}kick\` \`${prefix}ban\` \`${prefix}warn\` \`${prefix}slowmode\``, inline: true },
-	    )
+	.addFields(
+	    { name: 'General Commands', value: `\`${prefix}serverinfo\` \`${prefix}userinfo\` \`${prefix}8ball\` \`${prefix}coinflip\` \`${prefix}say\``, inline: true },
+	    { name: 'Moderation Commands', value: `\`${prefix}clear\` \`${prefix}kick\` \`${prefix}ban\` \`${prefix}warn\` \`${prefix}slowmode\``, inline: true },
+	)
         .addField('Admin Commands', `\`${prefix}leave\` \`${prefix}enable\` \`${prefix}disable\``, true)
         .addField(`Support Commands`, `\`${prefix}help\` \`${prefix}report\` \`${prefix}suggest\` \`${prefix}links\` \`${prefix}stats\``)
         .setTimestamp()
-	    .setFooter(`Requested By ${member.user.tag}`)
+	.setFooter(`Requested By ${member.user.tag}`)
 
         if(args[0]){
             const embed = new Discord.MessageEmbed()
@@ -49,8 +49,7 @@ module.exports = {
                 embed.addFields({ name: `__Permissions:__`, value: `${permissionString}`, inline: true })
             }
             message.channel.send(embed).catch(() => { message.author.send(`I am unable to send messages in ${message.channel}, please move to another channel`)}).catch(() => { })
-
-
+		
         } else{
             message.channel.send(realhelp).catch(() => { message.author.send(`I am unable to send messages in ${message.channel}, please move to another channel`)}).catch(() => { })
         }
