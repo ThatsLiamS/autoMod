20cc8a6883b9e456c556edad5d51e776d4e8f1e7
ThatsLiamS <73022526+ThatsLiamS@users.noreply.github.com>
2021-05-03 18:44:52 +0100

    Update kick.js

--- src/commands/moderation/kick.js
+++ src/commands/moderation/kick.js
@@ -12,27 +12,29 @@ module.exports = {
         if (!member.hasPermission("KICK_MEMBERS")) return message.reply("You do not have permission to use this command, if it is a mistake please contact the server owner").catch(() => { message.author.send(`I am unable to send messages in ${message.channel}, please move to another channel`)}).catch(() => { })
         if(!message.guild.me.hasPermission("KICK_MEMBERS") && !message.guild.me.hasPermission("ADMINISTRATOR")) return message.reply("I do have not permission to kick members").catch(() => { message.author.send(`I am unable to send messages in ${message.channel}, please move to another channel`)}).catch(() => { })
         
-        const user = message.mentions.members.first();
-        if (!user) return message.reply(`Please include the user to kick`).catch(() => { message.author.send(`I am unable to send messages in ${message.channel}, please move to another channel`)}).catch(() => { })
+        if(!args[0]) return message.reply(`Please include a member to ban`)
         
-        if(user.id == message.guild.owner.id) return message.reply("I'm sorry but you can not kick the owner of the server!").catch(() => { message.author.send(`I am unable to send messages in ${message.channel}, please move to another channel`)}).catch(() => { })
-        if(user.id == message.author.id) return message.reply("You can not kick yourself").catch(() => { message.author.send(`I am unable to send messages in ${message.channel}, please move to another channel`)}).catch(() => { })
+        const targetMember = message.guild.members.cache.get(args[0]) || message.mentions.members.first();
+        if (!targetMember) return message.reply(`Please include the user to kick`).catch(() => { message.author.send(`I am unable to send messages in ${message.channel}, please move to another channel`)}).catch(() => { })
+        
+        if(targetMember.id == message.guild.owner.id) return message.reply("I'm sorry but you can not kick the owner of the server!").catch(() => { message.author.send(`I am unable to send messages in ${message.channel}, please move to another channel`)}).catch(() => { })
+        if(targetMember.id == message.author.id) return message.reply("You can not kick yourself").catch(() => { message.author.send(`I am unable to send messages in ${message.channel}, please move to another channel`)}).catch(() => { })
  
         let reason = args.slice(1).join(" ");
         if (reason.length < 1) reason = "No reason specified"
         
         let errorMessage = false
-        user.kick({reason: `${reason}`}).catch(() => {
-            message.channel.send("I am unable to kick that Member").catch(() => { message.author.send(`I am unable to send messages in ${message.channel}, please move to another channel`)}).catch(() => { })
+        targetMember.kick({reason: `${reason}`}).catch(() => {
+            message.channel.send(`Sorry, an error occured when trying to kick ${targetMember.user.tag}`).catch(() => { message.author.send(`I am unable to kick that member.`)}).catch(() => { })
             errorMessage = true
         }).then(() => {
 
             const channelkicked = new Discord.MessageEmbed()
             .setColor('#DC143C')
             .setTitle(`A Member Was Kicked`)
-            .setThumbnail(user.user.displayAvatarURL())
+            .setThumbnail(targetMember.user.displayAvatarURL())
             .addFields(
-              { name: '**Who?**', value: `${user}|${user.user.tag}`, inline: true },
+              { name: '**Who?**', value: `${targetMember}\n${targetMember.user.tag}`, inline: true },
               { name: '**Reason**', value: `${reason}`, inline: true },
             )
             .setTimestamp()
@@ -40,9 +42,6 @@ module.exports = {
         
             if(errorMessage == true) return
             message.channel.send(channelkicked).catch(() => {})
-
-            client.developer.get('logs').execute(client, message, "Kick Command")
-            return;
         })
     }
 }
