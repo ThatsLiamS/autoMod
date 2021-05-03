da8fdcd2458141e02092a030552079026de1b303
ThatsLiamS <73022526+ThatsLiamS@users.noreply.github.com>
2021-05-03 12:27:55 +0100

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -4,13 +4,15 @@ module.exports = {
     once: true,
     async execute(client) {
 
-        await client.user.setPresence({ status: "online", 
-            activity:{ 
-                name: `Over Your Server`, 
-                type: `WATCHING` 
-            } 
+        await client.shard.fetchClientValues('guilds.cache.size').then(results => {
+            serverCount = results.reduce((acc, guildCount) => acc + guildCount, 0)
         })
+
+        const time = new Date()
+        const startTime = `${time.getHours()}:${time.getMinutes()}, ${time.getDate()}/${time.getMonth()}/${time.getFullYear()} UTC`
+
+        await client.user.setPresence({ status: "online", activity:{ name: `Over Your Server`, type: `WATCHING` } })
         
-        await console.log(`Logged in as ${client.user.tag}!\n looking over ${client.guilds.cache.size} servers`)
+        console.log(`Last restart: ${startTime}\n\nLogged in as ${client.user.tag}! looking over ${serverCount} servers`)
     }
 }
