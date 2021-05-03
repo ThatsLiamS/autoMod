c537be2d8d1bf3e37189c1b77f2416ebc7cf9824
ThatsLiamS <73022526+ThatsLiamS@users.noreply.github.com>
2021-05-03 18:48:57 +0100

    Update slowmode.js

--- src/commands/moderation/slowmode.js
+++ src/commands/moderation/slowmode.js
@@ -9,6 +9,7 @@ module.exports = {
         
         if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.reply("You do not have permission to use this command, if it is a mistake please contact the server owner").catch(() => { })
         if(!message.guild.me.hasPermission("MANAGE_CHANNELS") && !message.guild.me.hasPermission("ADMINISTRATOR")) return message.reply("I do have not permission to manage the channel").catch(() => { })
+        
         if(!args[0]) return message.reply("Please include how long you want slowmode for").catch(() => { })
 
         if(args[0] == "off"){
@@ -18,7 +19,6 @@ module.exports = {
             }catch{
                 message.reply("**I am unable to turn the slowmode off.**").catch(() => { })
             }
-            return client.developer.get('logs').execute(client, message, "Slowmode Command")
         }
 
         if (isNaN (args[0])) return message.reply("Please provide a number to the slowmode to.").catch(() => {})
@@ -27,7 +27,6 @@ module.exports = {
         try{
             message.channel.setRateLimitPerUser(args[0])
             message.channel.send(`Sucessfully set the slowmode in ${message.channel} to ${args[0]}!`).catch(() => {})
-            client.developer.get('logs').execute(client, message, "Slowmode Command")
         }catch{
             message.channel.send(`**I am unable to set the slowmode to ${number}, sorry ${message.member}!`).catch(() => {})
         }
