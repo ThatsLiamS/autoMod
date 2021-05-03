b9eba1cf25891609a4126239ad941c5521cc3d1d
ThatsLiamS <73022526+ThatsLiamS@users.noreply.github.com>
2021-05-03 11:33:08 +0100

    Update enable.js

--- src/commands/admin/enable.js
+++ src/commands/admin/enable.js
@@ -10,23 +10,21 @@ for (const file of files) {
 
 module.exports = {
     name: 'enable',
-    description: "Provides the ability to toggle certain features on, to help suit your requirements!",
+    description: "Provides the ability to toggle certain features on, to help suit your server!\n\nThe features are: `word-filter` and `ghost-ping`",
     aliases: [],
     usage: '<feature> <channel>',
     permissions: ["Admin"],
     async execute(message, args, prefix, client, firestore){
-        client.developer.get('logs').execute(client, message, "Enable Command")
 
-        let member = message.member
+        const member = await message.member
         if(member.hasPermission('ADMINISTRATOR')){
 
-            if(!args[0]) return message.reply(`Please include the feature you wish to enable`)
+            if(!args[0]) return message.reply(`Please include the features you want to disable! To view the features do \`${prefix}help enable\``)
 
             if(args[0] == "word-filter") enable.get('wordFilter').execute(message, args, prefix, client, firestore)
+            if(args[0] == "ghost-ping") enable.get('ghostPing').execute(message, args, prefix, client, firestore)
             
-            else{
-                return message.reply('That is not a valid feature! The features are: `word-filter`')
-            }       
+            else return message.reply('That is not a valid feature! To view the features do \`${prefix}help enable\`')      
         }
         else{
             message.reply(`:no: You do not have permission to use this command.`)
