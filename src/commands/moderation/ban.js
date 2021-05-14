23f20dc4bea647b202299945f618455fd4a24864
ThatsLiamS <73022526+ThatsLiamS@users.noreply.github.com>
2021-05-14 20:59:29 +0100

    Update ban.js

--- src/commands/moderation/ban.js
+++ src/commands/moderation/ban.js
@@ -9,17 +9,12 @@ module.exports = {
 
         const member = message.member;
 
-        if (!member.hasPermission("BAN_MEMBERS")) return message.reply("You do not have permission to use this command, if it is a mistake please contact the server owner").catch(() => { message.author.send(`I am unable to send messages in ${message.channel}, please move to another channel`)}).catch(() => { })
-        if(!message.guild.me.hasPermission("BAN_MEMBERS") && !message.guild.me.hasPermission("ADMINISTRATOR")) return message.reply("I do have not permission to ban members").catch(() => { message.author.send(`I am unable to send messages in ${message.channel}, please move to another channel`)}).catch(() => { })
-        
         if(!args[0]) return message.reply(`Please include a member to ban`)
         
         const targetMember = message.guild.members.cache.get(args[0]) || message.mentions.members.first();
         if (!targetMember) return message.reply(`Please include a Member to ban`).catch(() => { message.author.send(`I am unable to send messages in ${message.channel}, please move to another channel`)}).catch(() => { })
 
-        if(targetMember.id == message.guild.owner.id) return message.reply("I'm sorry but you can not ban the owner of the server!").catch(() => { message.author.send(`I am unable to send messages in ${message.channel}, please move to another channel`)}).catch(() => { })
         if(targetMember.id == message.author.id) return message.reply("You can not ban yourself").catch(() => { message.author.send(`I am unable to send messages in ${message.channel}, please move to another channel`)}).catch(() => { })
-   
         if(!targetMember.banable) return message.reply(`I am unable to ban ${targetMember.user.tag}`)
 
         let reason = args.slice(1).join(" ");
@@ -34,13 +29,13 @@ module.exports = {
             const channelBanned = new Discord.MessageEmbed()
             .setColor('#DC143C')
             .setTitle(`A Member Was Banned`)
+            .setAuthor(`${message.member.user.tag}`, `${message.member.user.displayAvatarURL()}`)
             .setThumbnail(user.user.displayAvatarURL())
             .addFields(
-                { name: '**Who?**', value: `${targetMember}\${targetMember.user.tag}`, inline: true },
+                { name: '**Who?**', value: `${targetMember}\n${targetMember.user.tag}`, inline: true },
                 { name: '**Reason**', value: `${reason}`, inline: true },
             )
             .setTimestamp()
-            .setFooter(`Ban was requested by @${member.user.tag} | Bot Created By @ThatsLiamS#3813\n`)
             
             if(errorMessage == true) return
             message.channel.send(channelBanned).catch(() => {})
