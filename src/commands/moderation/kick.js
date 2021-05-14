61351f23b72c34ef6d2576fc882f16183327f296
ThatsLiamS <73022526+ThatsLiamS@users.noreply.github.com>
2021-05-14 21:04:26 +0100

    Update kick.js

--- src/commands/moderation/kick.js
+++ src/commands/moderation/kick.js
@@ -4,20 +4,17 @@ module.exports = {
     description: "Temporarily removes a member from the server!\nNote: They can rejoin if they have a invite code",
     usage: '<@member> [reason]',
     permissions: ["Kick Members"], 
+    myPermissions: ['Kick Members', 'Send Messages', 'Embed Links'],
     aliases: ["k"],
     async execute(message, args, prefix, client, firestore){
  
         const member = message.member;
-
-        if (!member.hasPermission("KICK_MEMBERS")) return message.reply("You do not have permission to use this command, if it is a mistake please contact the server owner").catch(() => { message.author.send(`I am unable to send messages in ${message.channel}, please move to another channel`)}).catch(() => { })
-        if(!message.guild.me.hasPermission("KICK_MEMBERS") && !message.guild.me.hasPermission("ADMINISTRATOR")) return message.reply("I do have not permission to kick members").catch(() => { message.author.send(`I am unable to send messages in ${message.channel}, please move to another channel`)}).catch(() => { })
-        
+ 
         if(!args[0]) return message.reply(`Please include a member to ban`)
         
         const targetMember = message.guild.members.cache.get(args[0]) || message.mentions.members.first();
         if (!targetMember) return message.reply(`Please include the user to kick`).catch(() => { message.author.send(`I am unable to send messages in ${message.channel}, please move to another channel`)}).catch(() => { })
         
-        if(targetMember.id == message.guild.owner.id) return message.reply("I'm sorry but you can not kick the owner of the server!").catch(() => { message.author.send(`I am unable to send messages in ${message.channel}, please move to another channel`)}).catch(() => { })
         if(targetMember.id == message.author.id) return message.reply("You can not kick yourself").catch(() => { message.author.send(`I am unable to send messages in ${message.channel}, please move to another channel`)}).catch(() => { })
  
         let reason = args.slice(1).join(" ");
@@ -32,13 +29,13 @@ module.exports = {
             const channelkicked = new Discord.MessageEmbed()
             .setColor('#DC143C')
             .setTitle(`A Member Was Kicked`)
+            .setAuthor(`${message.member.user.tag}`, `${message.member.user.displayAvatarURL()}`)
             .setThumbnail(targetMember.user.displayAvatarURL())
             .addFields(
               { name: '**Who?**', value: `${targetMember}\n${targetMember.user.tag}`, inline: true },
               { name: '**Reason**', value: `${reason}`, inline: true },
             )
             .setTimestamp()
-            .setFooter(`Kick Was Requested By @${member.user.tag} | Bot Created By @ThatsLiamS#3813 and Team\n`)
         
             if(errorMessage == true) return
             message.channel.send(channelkicked).catch(() => {})
