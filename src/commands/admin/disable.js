e15eec6f2b09297585d4329201bf40c78ddd2411
ThatsLiamS <73022526+ThatsLiamS@users.noreply.github.com>
2021-05-14 20:49:18 +0100

    Update disable.js

--- src/commands/admin/disable.js
+++ src/commands/admin/disable.js
@@ -15,19 +15,13 @@ module.exports = {
     permissions: ["administrator"],
     async execute(message, args, prefix, client, firestore){
 
-        const member = message.member
-        if(member.hasPermission('ADMINISTRATOR')){
             
-            if(!args[0]) return message.reply(`Please include the features you want to disable! To view the features do \`${prefix}help disable\``)
+        if(!args[0]) return message.reply(`Please include the features you want to disable! To view the features do \`${prefix}help disable\``)
 
-            if(args[0] == "word-filter") disable.get('wordFilter').execute(message, args, prefix, client, firestore)
-            if(args[0] == "ghost-ping") disable.get('ghostPing').execute(message, args, prefix, client, firestore)
+        if(args[0] == "word-filter") disable.get('wordFilter').execute(message, args, prefix, client, firestore)
+        else if(args[0] == "ghost-ping") disable.get('ghostPing').execute(message, args, prefix, client, firestore)
             
-            else return message.reply(`That is not a valid feature! To view the features do \`${prefix}help disable\``)
+        else return message.reply(`That is not a valid feature! To view the features do \`${prefix}help disable\``)
    
-        }
-        else{
-            message.reply(`:no: You do not have permission to use this command.`)
-        }
     }
 }
