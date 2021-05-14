c485a739d2be296df8deaaad7f14fbad0112d045
ThatsLiamS <73022526+ThatsLiamS@users.noreply.github.com>
2021-05-14 20:50:52 +0100

    Update leave.js

--- src/commands/admin/leave.js
+++ src/commands/admin/leave.js
@@ -2,13 +2,12 @@ const Discord = require('discord.js');
 module.exports = {
     name: 'leave',
     description: "Forces autoMod to leave the server :sob:",
-    permissions: ["Server Owner"],
+    ownerOnly: true,
     async execute(message){
-        if (message.guild.ownerID !== message.author.id) return message.reply(`Sorry, you must be the server owner to use this commmand!`).catch(() => { message.author.send(`I am unable to send messages in that channel, please switch to a different channel.`)}).catch(() => { })
-
+        
         const embed = new Discord.MessageEmbed()
         .setTitle("Good Bye!")
-        .setDescription("Goodbye @everyone, I am sorry I could no longer be of service to you.\n\nIf you ever want to invite me to your own server or back to this one, click [here](https://automod.web.app/invite)")
+        .setDescription("Goodbye everyone, I am sorry I could no longer be of service to you.\n\nIf you ever want to invite me to your own server or back to this one, click [here](https://automod.web.app/invite)")
         await message.channel.send(embed).catch(() => { message.author.send(`Goodbye! You will be missed`)}).catch(() => { })
         message.guild.leave()
     }
