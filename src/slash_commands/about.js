38f61509108b69131933f192edb6876aeb44ead1
Liam Skinner <liamdavidskinner@gmail.com>
2021-07-11 18:00:57 +0100

    Changed about command UI

--- src/slash_commands/about.js
+++ src/slash_commands/about.js
@@ -2,37 +2,80 @@ const Discord = require("discord.js");
 const { developers, bot } = require(`${__dirname}/../util/values`);
 
 const prefix = '!';
+
 const getMemberCount = () => {
 	return this.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0);
 };
+
+function fitString(str, length) {
+	length -= str.length;
+	return str + " ".repeat(length);
+}
+
+function makeGrid(results) {
+	const length = [8, 7, 7];
+
+	const border = '+----+----------+----------+---------+';
+	const title = '| ID |   PING​   | SERVERS  |  US​ERS  |';
+	let rows = [];
+
+	for(let y = 0; y < results[0].length; y++) {
+		let values = [y + 1];
+
+		for(let x = 0; x < 3; x++) {
+			if(x == '0') {
+				let num = results[x][y];
+				num = num.toString();
+				if(num.length == 2) { num = 0 + num; }
+
+				values.push(`${num} ms  `);
+			}
+			else { values.push(fitString(results[x][y].toString(), length[x])); }
+		}
+		rows[y] = `| ${values[0]}  |  ${values[1]}|   ${values[2]}|  ${values[3]}|`;
+	}
+
+	let grid = `${border}\n${title}\n${border}\n`;
+	for(const row of rows) { grid += `${row}\n`; }
+	grid += `${border}`;
+
+	return grid;
+}
 
 module.exports = {
 	name: "about",
 	description: "Slash command that shows cool information about the bot!",
 	async execute(interaction, client) {
 
-		const ping = Math.round(client.ws.ping);
-		const uptime = `${Math.floor(client.uptime / 86400000)}d ${Math.floor(client.uptime / 3600000) % 24}h ${Math.floor(client.uptime / 60000) % 60}m ${Math.floor(client.uptime / 1000) % 60}s\``;
+		await interaction.defer({ ephemeral: false });
+
+		const uptime = `${Math.floor(client.uptime / 86400000)}d ${Math.floor(client.uptime / 3600000) % 24}h ${Math.floor(client.uptime / 60000) % 60}m ${Math.floor(client.uptime / 1000) % 60}s`;
 
 		const promises = [
+			client.shard.fetchClientValues('ws.ping'),
 			client.shard.fetchClientValues('guilds.cache.size'),
 			client.shard.broadcastEval(getMemberCount),
 		];
 		const results = await Promise.all(promises);
 
-		const servers = results[0].reduce((acc, guildCount) => acc + guildCount, 0);
-		const users = results[1].reduce((acc, memberCount) => acc + memberCount, 0);
+		const servers = results[1].reduce((acc, guildCount) => acc + guildCount, 0);
+		const users = results[2].reduce((acc, memberCount) => acc + memberCount, 0);
 
 		const shard = `# ${interaction.guild.shardID + 1} out of ${client.shard.count}`;
 
 		const embed = new Discord.MessageEmbed()
 			.setTitle("My Information")
 			.setColor(`GREEN`)
-			.setDescription(`Hey, I'm **[${bot.tag}](${bot.invite})**! My prefix is: \`${prefix}\`\nYou can also mention me as a prefix!`)
+			.setDescription(`Hey, I'm **[${bot.tag}](${bot.invite})**! My prefix is: \`${prefix}\`\nYou can also mention me as a prefix!\n\n\`\`\`\n${makeGrid(results)}\`\`\``)
 			.addFields(
-				{ name: `**Developer:**`, value:`[${developers.ThatsLiamS.tag}](${developers.ThatsLiamS.profile})`, inline: false },
-				{ name: `**Discord Stats:**`, value:`Servers: ${servers}\nUsers: ${users}`, inline: false },
-				{ name: `**Bot's Stats:**`, value:`Ping: \`${ping}ms\`\nUptime: \`${uptime}\n\nShard: ${shard}`, inline: false },
+
+				{ name: `**Total Servers:**`, value:`${servers}`, inline: true },
+				{ name: `**Total Users:**`, value: `${users}`, inline: true },
+				{ name: `**Total Commands:**`, value: `18`, inline: true },
+
+				{ name: `**Developer:**`, value:`**[${developers.ThatsLiamS.tag}](${developers.ThatsLiamS.profile})**`, inline: true },
+				{ name: `**Uptime:**`, value:`\`${uptime}\``, inline: true },
+				{ name: `**Shard ID:**`, value:`\`${shard}\``, inline: true },
 			);
 
 		await interaction.reply({ embeds: [embed] });
