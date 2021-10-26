02631a2286b1a0eedf73864b73eb4c40ca680c45
Liam Skinner <liamdavidskinner@gmail.com>
2021-10-27 00:51:18 +0100

    Added code structure

---
+++ src/index.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const { ShardingManager } = require('discord.js');
+
+const manager = new ShardingManager('./bot.js', {
+	totalShards: 'auto',
+	token: process.env['BotToken']
+});
+
+manager.on('shardCreate', shard => {
+	console.log(`Launched shard ${shard.id + 1}/${this.totalShards}`);
+});
+
+manager.spawn(this.totalShards, 500, -1);
