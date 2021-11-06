bc784a82e892f11f23255571d9cba186184c2d44
Liam Skinner <liamdavidskinner@gmail.com>
2021-11-06 18:24:06 +0000

    Completed fun commands

---
+++ src/commands/fun/coinflip.js
@@ -0,0 +1,38 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+	name: 'coinflip',
+	description: 'Flip a coin!',
+	usage: '',
+
+	permissions: [],
+	ownerOnly: false,
+
+	error: false,
+	execute: ({ interaction }) => {
+
+		const embed = new MessageEmbed()
+			.setColor('#CD7F32')
+			.setAuthor(interaction.member.user.username, interaction.member.displayAvatarURL())
+			.setTitle('Coin Flipper!')
+			.setFooter('Powered by Coin Flipper#1767')
+			.setTimestamp();
+
+		const random = Math.floor(Math.random() * 100);
+		if (random > 50) {
+			embed.setDescription('The coin landed on heads!')
+				.setThumbnail('https://assets.liamskinner.co.uk/coin/heads.png');
+		}
+		else if (random < 50) {
+			embed.setDescription('The coin landed on tails!')
+				.setThumbnail('https://assets.liamskinner.co.uk/coin/tails.png');
+		}
+		else {
+			embed.setDescription('The coin landed on its side, very impressive!')
+				.setThumbnail('https://assets.liamskinner.co.uk/coin/side.png');
+		}
+
+		interaction.reply({ embeds: [embed], ephemeral: true });
+
+	},
+};
