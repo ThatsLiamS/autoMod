dad09490e94d4f064f7b1cfff1fa653cd7c1ef1a
Liam Skinner <liamdavidskinner@gmail.com>
2021-11-07 21:44:03 +0000

    Changed shard spawning

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 require('dotenv').config();
 const { ShardingManager } = require('discord.js');
 
-const manager = new ShardingManager('./bot.js', {
+const manager = new ShardingManager('./src/bot.js', {
 	totalShards: 'auto',
 	token: process.env['BotToken'],
+	respawn: true,
 });
 
-manager.on('shardCreate', shard => {
-	console.log(`Launched shard ${shard.id + 1}/${this.totalShards}`);
+manager.on('shardCreate', (shard) => {
+	console.log(`Launched shard ${shard.id + 1}/${manager.totalShards}`);
 });
 
-manager.spawn(this.totalShards, 500, -1);
+manager.spawn({ amount: this.totalShards, delay: 500, timeout: -1 });
