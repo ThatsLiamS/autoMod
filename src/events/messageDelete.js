fb81b1a528a973fc11b1e7210856f4ac51c8ff5d
Liam Skinner <liamdavidskinner@gmail.com>
2022-04-06 23:48:49 +0100

    refactor(GhostPing): added .map() and overall logic

--- src/events/messageDelete.js
+++ src/events/messageDelete.js
@@ -1,4 +1,6 @@
 const { MessageEmbed } = require('discord.js');
+const { filter, validate } = require('./../utils/ghostping');
+
 const defaultData = require('./../utils/defaults').guilds;
 
 module.exports = {
@@ -12,29 +14,27 @@ module.exports = {
 		/* Ghost Ping Detector */
 		if (message.mentions.members.size !== 0 || message.mentions.roles.size !== 0) {
 
-			const mentions = [];
+			if (message.author.bot || message.mentions.members.size == 0 && message.mentions.roles.size == 0) return false;
 
-			message.mentions.members.forEach((member) => {
-				if (!member.user.bot && member.id != message.author.id) mentions.push(member.toString());
-			});
-			message.mentions.roles.forEach((role) => mentions.push(role.toString()));
+			let mentions = message.mentions.members.map(member => validate(member, message));
+			mentions = [...message.mentions.roles.map(x => filter(x)), ...mentions];
 
 			if (mentions != []) {
-
-				const embed = new MessageEmbed()
-					.setTitle('Ghost Ping Detected')
-					.setColor('#C0C0C0')
-					.addFields(
-						{ name: '__Who?__', value: `**Author:** ${message.author}\n**Channel:** ${message.channel}`, inline: true },
-						{ name: '__Mentions__', value: `${mentions.join(' ')}!`, inline: true },
-					)
-					.setFooter('Do not ghost ping, that\'s mean.')
-					.setTimestamp();
 
 				const collection = await firestore.collection('guilds').doc(message.guild.id).get();
 				const userData = collection.data() || defaultData;
 
 				if (userData['ghost ping']['on'] == true) {
+
+					const embed = new MessageEmbed()
+						.setTitle('Ghost Ping Detected')
+						.setColor('#C0C0C0')
+						.addFields(
+							{ name: '__Who?__', value: `**Author:** ${message.author}\n**Channel:** ${message.channel}`, inline: true },
+							{ name: '__Mentions__', value: `${mentions.join(' ')}!`, inline: true },
+						)
+						.setFooter({ text: 'Do not ghost ping, that\'s mean.' })
+						.setTimestamp();
 
 					const channel = client.channels.cache.get(userData['ghost ping']['channel']);
 					channel.send({ embeds: [embed] });
